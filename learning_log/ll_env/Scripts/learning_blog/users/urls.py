c23c69f051a289f752f6a31b60b732ccb3d9903a
ehmatthes <eric@example.com>
2020-04-23 19:01:21 +0800

    first commit

---
+++ learning_log/ll_env/Scripts/learning_blog/users/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    #µÇÂ¼Ò³Ãæ
+    url(r'^login/$',login,{'template_name':'users/login.html'},
+        name='login'),
+    url(r'^logout/$',views.logout_view,name='logout'),
+    url(r'^register/$',views.register,name="register"),
+]
+
